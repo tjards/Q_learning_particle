8554c6d7e67f29a5a8728d8604024eb49692a5a7
tjards <travis.jardine@gmail.com>
2021-05-21 21:40:58 -0400

    initial inset plots

--- main.py
+++ main.py
@@ -30,14 +30,15 @@ writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 import qLearnLib as QL
 from numpy import linalg as LA
 import random
+from mpl_toolkits.axes_grid.inset_locator import (inset_axes, InsetPosition, mark_inset)
 
 #%% Simulation Setup
 # --------------------
  
 Ti = 0.0        # initial time
-Tf = 1000         # final time 
+Tf = 500         # final time 
 Ts = 0.1        # sample time
-Tz = 0.005      # integration step size
+Tz = 0.01      # integration step size
 
 state   = np.array([1.9, 0.1, 1, 0.2, 0.3, 0.4])   # format: [x, xdot, y, ydot, z, zdot]
 inputs  = np.array([0.21, 0.15, 0.1])              # format: [xddot, yddot, zddot]
@@ -64,6 +65,7 @@ rewards_all[0,:] = reward
 costs_all      = np.zeros([nSteps, 1])  # to store costs
 costs_all[0,:] = trial_cost
 
+
 # initialize Q table
 nParams     = 2    # number of parameters to tune
 nOptions    = 10   # number of options to selection from (ranges between 0 and nOptions)
@@ -79,9 +81,8 @@ kp_i = QL.select(Q,0,nOptions,explore_rate)
 kp = scale*kp_i
 kd_i = QL.select(Q,1,nOptions,explore_rate)
 kd = scale*kd_i
-
-
-
+explore_rates_all       = np.zeros([nSteps, 1])  # to store explore rates
+explore_rates_all[0,:]  = explore_rate
 
 
 #%% Define the agent dynamics
@@ -113,7 +114,8 @@ while round(t,3) < Tf:
     states_all[i,:]     = state
     targets_all[i,:]    = target
     rewards_all[i,:]    = reward #1/np.maximum(trial_cost,0.00001)      
-    costs_all[i,:]      = trial_cost #1/np.maximum(trial_cost,0.00001)                      
+    costs_all[i,:]      = trial_cost #1/np.maximum(trial_cost,0.00001)
+    explore_rates_all[i,:]  = explore_rate                      
 
     # increment 
     t += Ts
@@ -154,10 +156,10 @@ while round(t,3) < Tf:
         trial_counter = 0
         
         # select new target (randomly)
-        #target  = 10*np.array([random.uniform(-1, 1),random.uniform(-1, 1),random.uniform(-1, 1)]) 
+        target  = 10*np.array([random.uniform(-1, 1),random.uniform(-1, 1),random.uniform(-1, 1)]) 
         
         # reduce the explore rate
-        explore_rate = 0.995**t
+        explore_rate = 0.99**t
         #print(explore_rate)
         
 
@@ -174,6 +176,8 @@ while round(t,3) < Tf:
 # %% Animate
 # ---------- 
 
+#%% trajectory
+
 fig = plt.figure()
 ax = p3.Axes3D(fig)
 
@@ -186,25 +190,116 @@ ax.set_zlim3d([-axis, axis])
 line, = ax.plot([], [],[], 'bo-',ms=10, lw=2)
 line_target, = ax.plot([], [],[], 'ro-', ms=5, lw=2)
 
-time_template = 'Time = %.1fs'
+time_template = 'Time = %.1f/%.0fs'
 #reward_template = 'Last Reward = %.1f'
 time_text = ax.text2D(0.05, 0.95, '', transform=ax.transAxes)
-time_text2 = ax.text2D(0.65, 0.95, 'Double Integrator Kinematics', transform=ax.transAxes)
+time_text2 = ax.text2D(0.65, 0.95, 'Q-Learning Control', transform=ax.transAxes)
 time_text3 = ax.text2D(0.65, 0.90, 'Controller: PD', transform=ax.transAxes)
 #text_reward = ax.text2D(0.05, 0.90, '', transform=ax.transAxes)
+
+# create inset axis
+#==================
+ax4 = plt.axes([0,0,1,1])
+# set position manually (x pos, y pos, x len, y len)
+#ip = InsetPosition(ax, [0.58,0.58,0.4,0.4])
+ip = InsetPosition(ax, [0,0.7,0.2,0.2])
+ax4.set_axes_locator(ip)
+# cool, make lines to point (save this for later)
+#mark_inset(ax2, ax3, loc1=2, loc2=4, fc="none", ec='0.5')
+# data
+line2, = ax4.plot([], [], '--', c='b', mew=2, alpha=0.8,label='Cost')
+#ax4.plot([], [], '-', c='g', mew=2, alpha=0.8,label='Explore rate')
+
+
+# create inset axis
+#==================
+ax5 = ax4.twinx()
+#ax5 = plt.axes([0,0,1,1])
+# set position manually (x pos, y pos, x len, y len)
+#ip2 = InsetPosition(ax, [0.18,0.18,0.4,0.4])
+ax5.set_axes_locator(ip)
+# cool, make lines to point (save this for later)
+#mark_inset(ax2, ax3, loc1=2, loc2=4, fc="none", ec='0.5')
+# data
+line3, = ax4.plot([], [], '-', c='g', mew=2, alpha=0.8,label='Explore')
+#ax4.plot([], [], '-', c='g', mew=2, alpha=0.8,label='Explore rate')
+
+
+
+# add another plot?
+#ax5 = plt.axes([0,0,1,1])
+#ax5.set_axes_locator(ip)
+#ax5.plot(t_all[1::int(Tl/Ts)],explore_rates_all[1::int(Tl/Ts),0],'--', c='g', mew=2, alpha=0.8,label='Explore Rate')
+
+#ax4.plot(t_all[1::int(Tl/Ts)],(1/float(max(costs_all)))*costs_all[1::int(Tl/Ts),0],'--', c='b', mew=2, alpha=0.8,label='Cost')
+#ax4.set_xlabel('Time [s]')
+ax4.set_ylabel('Cost')
+ax4.set_xlim(0,max(t_all))
+ax4.set_ylim(0,1)
+ax4.tick_params(axis='y', labelcolor='b')
+
+#ax5.set_xlabel('Time [s]')
+ax5.set_ylabel('Explore Rate')
+ax5.set_xlim(0,max(t_all))
+ax5.set_ylim(0,1)
+ax5.tick_params(axis='y', labelcolor='g')
+
 
 def update(i):
     line.set_data(states_all[i,0],states_all[i,2])
     line.set_3d_properties(states_all[i,4])
     line_target.set_data(targets_all[i,0],targets_all[i,1])
     line_target.set_3d_properties(targets_all[i,2])
-    time_text.set_text(time_template%(i*Ts))
+    time_text.set_text(time_template%(i*Ts,Tf))
     #text_reward.set_text(reward_template%rewards_all[i])
-    return line, time_text
+    line2.set_data(t_all[0:i],(1/float(max(costs_all[0:i])))*costs_all[0:i,0])
+    ax4.set_xlim(0,t_all[i]+1)
+    line3.set_data(t_all[0:i],(1/float(max(explore_rates_all[0:i])))*explore_rates_all[0:i,0])
+    ax5.set_xlim(0,t_all[i]+1)
+    return line, line2, line3, time_text
 
-ani = animation.FuncAnimation(fig, update, np.arange(1, len(states_all)),
-    interval=15, blit=False)
+#fast
+ani = animation.FuncAnimation(fig, update, np.arange(1, len(states_all)),interval=15, blit=False)
 
+#slow
+#ani2 = animation.FuncAnimation(fig, update, np.linspace(1, len(states_all), num=500, dtype=int),interval=15, blit=False)
+
+ani.save('animation.gif', writer=writer, progress_callback = lambda i, n: print(f'Saving frame {i} of {n}'))
 #ani.save('animation.gif', writer=writer)
+
+
 #plt.show()
 
+#%% Costs
+
+# plot costs
+fig2, ax2 = plt.subplots()
+plt.title('Q-Learning Control Parameters')
+ax2.plot(t_all[1::int(Tl/Ts)],costs_all[1::int(Tl/Ts),0],'-', c='b', mew=2, alpha=0.8,label='Cost')
+ax2.set_xlabel('Time [s]')
+ax2.set_ylabel('Cost [m]')
+#ax2.legend(loc=0)
+ax2.set_xlim(0,max(t_all))
+ax2.set_ylim(0,max(costs_all))
+
+# create inset axis
+ax3 = plt.axes([0,0,1,1])
+# set position manually (x pos, y pos, x len, y len)
+ip = InsetPosition(ax2, [0.55,0.55,0.4,0.4])
+ax3.set_axes_locator(ip)
+# cool, make lines to point (save this for later)
+#mark_inset(ax2, ax3, loc1=2, loc2=4, fc="none", ec='0.5')
+# data
+ax3.plot(t_all[1::int(Tl/Ts)],explore_rates_all[1::int(Tl/Ts),0],'--', c='g', mew=2, alpha=0.8,label='Explore Rate')
+ax3.set_xlabel('Time [s]')
+ax3.set_ylabel('Explore Rate')
+ax3.set_xlim(0,max(t_all))
+ax3.set_ylim(0,1)
+
+
+plt.savefig('cost.png')
+# other stuff
+#ax2.set_ylim(0,26)
+#ax2.set_yticks(np.arange(0,2,0.4))
+#ax3.set_xticklabels(ax2.get_xticks(), backgroundcolor='w')
+#ax3.tick_params(axis='x', which='major', pad=8)
